82d366d546d50d311c8f15a530a84c556a627dc1
Vladyslav Budko <vladyslav.budko@gmail.com>
2022-09-23 00:29:34 +0300

    9.2

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,3 +1,7 @@
+import flatpickr from 'flatpickr';
+// Дополнительный импорт стилей
+import 'flatpickr/dist/flatpickr.min.css';
+
 const refs = {
   pickerEl: document.querySelector('#datetime-picker'),
   startBtn: document.querySelector('[data-start]'),
@@ -6,49 +10,114 @@ const refs = {
   minutesEl: document.querySelector('[data-minutes]'),
   secondsEl: document.querySelector('[data-seconds]'),
 };
-refs.startBtn.addEventListener('click', startTimer);
-refs.pickerEl.addEventListener('click', picker);
 
-function picker(event) {
-  refs.pickerEl.setAttribute('type', 'datetime-local');
-  console.log(refs.pickerEl);
-}
+let isActive = false;
+refs.startBtn.style.color = '#390d0d34';
 
-function startTimer(event) {
-  console.log(refs.startBtn);
-}
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates, dateStr) {
+    console.log(selectedDates[0]);
+    console.log('Pаспродажа начнется:', dateStr);
+    const date = new Date();
+    console.log(date);
 
-// * - Адская копипаста со стека 💩
-   
-  function getTimeComponents(time) {
-    const hours = this.pad(
-      Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)),
-    );
-    const mins = this.pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
-    const secs = this.pad(Math.floor((time % (1000 * 60)) / 1000));
+    if (selectedDates[0] - date < 0) {
+      window.alert('Please choose a date in the future');
+      return;
+    };
 
-    return { hours, mins, secs };
+    startBtn.style = null;
+    isActive = true;
+  },
+};
+
+flatpickr('input', options);
+
+
+class Timer {
+  constructor({ onTick }) {
+    this.timerID = null;
+    this.isActive = false;
+    this.onTick = onTick;
   }
 
-  /*
-   * Принимает число, приводит к строке и добавляет в начало 0 если число меньше 2-х знаков
-   */
- function pad(value) {
-    return String(value).padStart(2, '0');
+  init() {
+    const time = convertMs(0);
+    this.onTick(time);
   }
+
+  startTimer() {
+    if (!this.isActive) {
+      return;
+    }
+
+    const startTime = Date.now();
+    setInterval(() => {
+      const currentTime = Date.now();
+      const deltaTime = startTime - currentTime;
+      const time = convertMs(deltaTime);
+      this.onTick(time);
+
+      if (time = 0) {
+        console.log("Распродажа началась!", startTime);
+        return;
+      } 
+    }, 1000);
+  }
+
+}
 
 const timer = new Timer({
   onTick: updateClockface,
 });
 
-refs.startBtn.addEventListener('click', timer.start.bind(timer));
-refs.stopBtn.addEventListener('click', timer.stop.bind(timer));
+refs.startBtn.addEventListener('click', timer.startTimer.bind(timer));
+
+
+// * - Адская копипаста со стека 💩
+
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = Math.floor(ms / day);
+  // Remaining hours
+  const hours = Math.floor((ms % day) / hour);
+  // Remaining minutes
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  // Remaining seconds
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
+
+console.log(convertMs(2000)); // {days: 0, hours: 0, minutes: 0, seconds: 2}
+console.log(convertMs(140000)); // {days: 0, hours: 0, minutes: 2, seconds: 20}
+console.log(convertMs(24140000)); // {days: 0, hours: 6 minutes: 42, seconds: 20}
+
+/*
+ * Принимает число, приводит к строке и добавляет в начало 0 если число меньше 2-х знаков
+ */
+function pad(value) {
+  return String(value).padStart(2, '0');
+}
 
 /*
  * - Принимает время в миллисекундах
- * - Высчитывает сколько в них вмещается часов/минут/секунд
+ * - Высчитывает сколько в них вмещается дней/часов/минут/секунд
  * - Рисует интерфейс
  */
-function updateClockface({ hours, mins, secs }) {
-  refs.clockface.textContent = `${hours}:${mins}:${secs}`;
+function updateClockface({ days, hours, minutes, seconds }) {
+  refs.daysEl.textContent = `${days}`;
+  refs.hoursEl.textContent = `${hours}`;
+  refs.minutesEl.textContent = `${minutes}`;
+  refs.secondsEl.textContent = `${seconds}`;
 }
