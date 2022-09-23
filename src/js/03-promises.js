a1c5cf0a72e491689c118a4a2890a7a2775a5241
Vladyslav Budko <vladyslav.budko@gmail.com>
2022-09-24 00:34:39 +0300

    9.3 -4

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,3 +1,5 @@
+import Notiflix from 'notiflix';
+
 const refs = {
   delayEl: document.querySelector('[name="delay"]'),
   stepEl: document.querySelector('[name="step"]'),
@@ -8,31 +10,57 @@ const refs = {
 refs.submitBtn.addEventListener('submit', start());
 
 function start(evt) {
-  evt.preventDefault();
+  // evt.preventDefault();
+
+  const AMOUNT = refs.amountEl.value;
+  const DELAY = refs.delayEl.value;
+  const STEP = refs.stepEl.value;
 
   const createPromise = ({ AMOUNT, DELAY, STEP }) => {
     return new Promise((resolve, reject) => {
+      let delay = DELAY;
+
       for (let position = 1; position <= AMOUNT; position += 1) {
-        let delay = DELAY;
         const shouldResolve = Math.random() > 0.3;
 
         setTimeout(() => {
           if (shouldResolve) {
-            resolve(`✅ Fulfilled promise ${position} in ${delay}ms`);
+            // resolve(`✅ Fulfilled promise ${position} in ${delay}ms`);
+            resolve(
+              Notiflix.Notify.success(
+                `✅ Fulfilled promise ${position} in ${delay}ms`
+              )
+            );
           }
-
-          reject(`❌ Rejected promise ${position} in ${delay}ms`);
+          // reject(`❌ Rejected promise ${position} in ${delay}ms`);
+          reject(
+            Notiflix.Notify.failure(
+              `❌ Rejected promise ${position} in ${delay}ms`
+            )
+          );
         }, delay);
         delay += STEP;
       }
     });
   };
 
-  const AMOUNT = refs.amountEl.value;
-  const DELAY = refs.delayEl.value;
-  const STEP = refs.stepEl.value;
-  createPromise({ AMOUNT, DELAY, STEP }).then().catch();
+  createPromise({ AMOUNT, DELAY, STEP })
+    .then(
+      Notiflix.Notify.success(`✅ Fulfilled promise `)
+    )
+    .catch(
+      Notiflix.Notify.failure(`❌ Rejected promise `)
+    );
 }
+
+// function createPromise(position, delay) {
+//   const shouldResolve = Math.random() > 0.3;
+//   if (shouldResolve) {
+//     // Fulfill
+//   } else {
+//     // Reject
+//   }
+// }
 
 // createPromise(2, 1500)
 //   .then(({ position, delay }) => {
