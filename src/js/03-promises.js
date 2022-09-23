756a5f098b68b392febef1e3c1dfc0ffcb432ee7
Vladyslav Budko <vladyslav.budko@gmail.com>
2022-09-24 02:52:54 +0300

    9.3-5

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -5,22 +5,23 @@ const refs = {
   stepEl: document.querySelector('[name="step"]'),
   amountEl: document.querySelector('[name="amount"]'),
   submitBtn: document.querySelector('[type="submit"]'),
+  formEl: document.querySelector('.form'),
 };
 
-refs.submitBtn.addEventListener('submit', start());
+refs.formEl.addEventListener('submit', start);
 
 function start(evt) {
-  // evt.preventDefault();
+  evt.preventDefault();
 
   const AMOUNT = refs.amountEl.value;
   const DELAY = refs.delayEl.value;
   const STEP = refs.stepEl.value;
 
-  const createPromise = ({ AMOUNT, DELAY, STEP }) => {
-    return new Promise((resolve, reject) => {
-      let delay = DELAY;
+  let delay = DELAY;
 
-      for (let position = 1; position <= AMOUNT; position += 1) {
+  for (let position = 1; position <= AMOUNT; position += 1) {
+    const createPromise = () => {
+      return new Promise((resolve, reject) => {
         const shouldResolve = Math.random() > 0.3;
 
         setTimeout(() => {
@@ -40,17 +41,19 @@ function start(evt) {
           );
         }, delay);
         delay += STEP;
-      }
-    });
-  };
+      });
+    };
 
-  createPromise({ AMOUNT, DELAY, STEP })
-    .then(
-      Notiflix.Notify.success(`✅ Fulfilled promise `)
-    )
-    .catch(
-      Notiflix.Notify.failure(`❌ Rejected promise `)
-    );
+    createPromise()
+      .then(
+        Notiflix.Notify.success(
+          `✅ Fulfilled promise ${position} in ${delay}ms`
+        )
+      )
+      .catch(
+        Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`)
+      );
+  }
 }
 
 // function createPromise(position, delay) {
